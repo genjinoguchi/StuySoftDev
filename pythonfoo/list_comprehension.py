2595f8791b719468f79f646896254e0bcf14982e
Genji Noguchi <genji.noguchi@gmail.com>
2014-09-29 12:30:14 -0400

    Advanced python list manipulation

---
+++ pythonfoo/list_comprehension.py
@@ -0,0 +1,29 @@
+"""
+Using lists in python / List Comprehension
+
+>>> [ x for x in range(10) }
+return [1,2,3,4,5,6,7,8,9]
+
+>>> [ 'z' for x in range(10) ]
+returns [ z.z.z.z.z.z.z]
+
+>>>[ x^2 for x in range(10) ]
+returns [squares]
+
+>>>[ ord(x) for x in p ]
+returns all the ascii values of the letters in the ilst.
+swag
+
+>>> [ x for x in range(10) if x%2 == 0 ]
+
+>>> [ x if x%2 == 0 else blah x in range(10)]
+
+>>> [ c for c in p if c in UC_LETTERS]waow
+
+>>> sum([1 if c in UC_LETTERS else 0 for c in p])
+
+>>>
+
+It's a different way of thinking-- you have the power to apply a function to an entire data set.
+
+"""
